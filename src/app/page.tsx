07d7d889516b81d55b3e260130829aad020a2cc2
dsamithmendis <samithmendis.01@gmail.com>
2025-10-19 19:54:33 +0530

    dev: home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import GalleryPage from "@/components/pages/cards";
-import UploadFruitCardPage from "@/components/pages/upload-fruit-cards";
-import UploadJuiceCardPage from "@/components/pages/upload-juice-cards";
 
 export default function Home() {
   return (
     <>
-      <UploadFruitCardPage />
-      <UploadJuiceCardPage />
       <GalleryPage />
     </>
   );
